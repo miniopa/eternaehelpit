4a7d2800037c3929af4b78f6dc6cd9d317784f7f
Thomas Miño <107081617+miniopa@users.noreply.github.com>
2026-06-30 10:24:47 -0300

    Add files via upload

---
+++ dist/server/assets/_tanstack-start-manifest_v-D4L-NlJl.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Root/Desktop/cyber-fix-friends-main(1)/cyber-fix-friends-main/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-CcGGnRSL.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-CcGGnRSL.js" } }] }, "/": { filePath: "C:/Users/Root/Desktop/cyber-fix-friends-main(1)/cyber-fix-friends-main/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-CNNLoSmT.js"] } } });
+export {
+  tsrStartManifest
+};
